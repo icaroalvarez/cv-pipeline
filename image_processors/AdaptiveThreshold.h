15110424c271ebc482ba342ec673e85b19532dda
Icaro Alvarez <36381262+icaroalvarez@users.noreply.github.com>
2020-02-15 22:11:51 +0100

    Remove gui_qt and image_processor subdirectories (now they are independent repos).

--- image_processors/AdaptiveThreshold.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ImageProcessor.h>
-
-class AdaptiveThreshold: public ImageProcessor {
-public:
-    AdaptiveThreshold();
-    void processImage(const cv::Mat &image) override;
-};
--- image_processors/Registerer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <PipelineController.h>
-
-class Registerer
-{
-public:
-    static void registerProcessors(PipelineController& pipelineController);
-};
