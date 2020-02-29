3edba06ca4969b4641ac7b438727c7c8e348db3b
Icaro Alvarez <36381262+icaroalvarez@users.noreply.github.com>
2020-02-29 11:23:00 +0100

    [Feature] Improve image frame source testing coverage.

--- test/ImageFrameSource.Test.cpp
+++ test/ImageFrameSource.Test.cpp
@@ -2,20 +2,47 @@
 #include <ImageFrameSource.h>
 #include <opencv2/imgcodecs.hpp>
 
-TEST_CASE("Loads the frame source")
+SCENARIO("Loads an image")
 {
-    ImageFrameSource frameSource;
+    GIVEN("An image frame source")
+    {
+        ImageFrameSource frameSource;
 
-    const std::string wrongSourcePath{"wrong_source_path"};
-    CHECK_THROWS_WITH(frameSource.loadFrom(wrongSourcePath),
-            "Unable to load image from: "+wrongSourcePath);
+        THEN("Frame index count is zero before loading the source")
+        {
+            CHECK(frameSource.framesCount() == 0);
+        }
 
-    const std::string imageSourcePath{fixtures_path"Lenna.png"};
-    REQUIRE_NOTHROW(frameSource.loadFrom(imageSourcePath));
+        THEN("When wrong source path is loaded")
+        {
+            const std::string wrongSourcePath{"wrong_source_path"};
+            CHECK_THROWS_WITH(frameSource.loadFrom(wrongSourcePath),
+                              "Unable to load image from: " + wrongSourcePath);
 
-    CHECK(frameSource.framesCount() == 1);
-    cv::Mat lennaImage{cv::imread(fixtures_path"Lenna.png")};
+        }
 
-    auto frame{frameSource.getFrameFromIndex(0)};
-    CHECK(std::equal(lennaImage.begin<uchar>(), lennaImage.end<uchar>(), frame.begin<uchar>()));
+        WHEN("An existing image path is loaded")
+        {
+            const std::string imageSourcePath{fixtures_path"Lenna.png"};
+            REQUIRE_NOTHROW(frameSource.loadFrom(imageSourcePath));
+
+            THEN("Frame index count is correct")
+            {
+                CHECK(frameSource.framesCount() == 1);
+            }
+
+            THEN("Throws getting an image with an incorrect index number")
+            {
+                CHECK_THROWS_WITH(frameSource.getFrameFromIndex(1),
+                                  "Unable to get frame from index 1. Index is out of bound (max index is 0)");
+            }
+
+            THEN("The image is retrieve with a correct index number")
+            {
+                cv::Mat lennaImage{cv::imread(fixtures_path"Lenna.png")};
+                auto frame{frameSource.getFrameFromIndex(0)};
+                CHECK(std::equal(lennaImage.begin<uchar>(), lennaImage.end<uchar>(), frame.begin<uchar>()));
+            }
+        }
+    }
 }
